b31b2117cb8be29a5b52fd74c54bcaedd1e5cc25
Martin Danielsson <mdanielsson75@gmail.com>
2022-04-15 20:01:41 +0200

    Move quantization to the engine
    Since the inference code depends on the quantization parameters, move
    the quantization to the engine so that everything is in one place.

--- serialize.py
+++ serialize.py
@@ -6,7 +6,7 @@ import numpy
 import struct
 import torch
 
-VERSION = 0x00000002
+VERSION = 0x00000003
 
   
 def write_header(buf):
@@ -14,37 +14,17 @@ def write_header(buf):
 
 
 def write_feature_transformer(buf, model):
-    # int16 bias = round(x * 127)
-    # int16 weight = round(x * 127)
     layer = model.input
     bias = layer.bias.data
-    bias = bias.mul(127).round().to(torch.int16)
     buf.extend(bias.flatten().numpy().tobytes())
-
     weight = model.combine_feature_weights()
-    weight = weight.mul(127).round().to(torch.int16)
     buf.extend(weight.transpose(0, 1).flatten().numpy().tobytes())
 
 
-def write_fc_layer(buf, layer, is_output=False):
-    # FC layers are stored as int8 weights, and int32 biases
-    kWeightScaleBits = 6
-    kActivationScale = 127.0
-    if not is_output:
-        kBiasScale = (1 << kWeightScaleBits) * kActivationScale # = 8128
-    else:
-        kBiasScale = 9600.0 # kPonanzaConstant * FV_SCALE = 600 * 16 = 9600
-    kWeightScale = kBiasScale / kActivationScale # = 64.0 for normal layers
-    kMaxWeight = 127.0 / kWeightScale # roughly 2.0
-
-    # int32 bias = round(x * kBiasScale)
-    # int8 weight = round(x * kWeightScale)
+def write_fc_layer(buf, layer):
     bias = layer.bias.data
-    bias = bias.mul(kBiasScale).round().to(torch.int32)
     buf.extend(bias.flatten().numpy().tobytes())
     weight = layer.weight.data
-    weight = weight.clamp(-kMaxWeight, kMaxWeight).mul(kWeightScale).round().to(torch.int8)
-    # Stored as [outputs][inputs], so we can flatten
     buf.extend(weight.flatten().numpy().tobytes())
 
 
@@ -66,7 +46,7 @@ def serialize(source, target):
     write_feature_transformer(buf, nnue)
     write_fc_layer(buf, nnue.l1)
     write_fc_layer(buf, nnue.l2)
-    write_fc_layer(buf, nnue.output, is_output=True)
+    write_fc_layer(buf, nnue.output)
 
     # Write converted model
     with open(target, 'wb') as f:
@@ -79,5 +59,5 @@ if __name__ == '__main__':
     parser.add_argument('target', help='Target file (.nnue)')
     args = parser.parse_args()
 
-    print('Converting %s to %s' % (args.source, args.target))
+#    print('Converting %s to %s' % (args.source, args.target))
     serialize(args.source, args.target)
