e147e5231b00b3996a48e3868d59f7c6048e5d45
Martin Danielsson <mdanielsson75@gmail.com>
2023-11-04 19:17:44 +0100

    Port to Pytorch 2.1

--- nnue_dataset.py
+++ nnue_dataset.py
@@ -54,8 +54,8 @@ class SparseBatch(ctypes.Structure):
         them = 1.0 - us
         outcome = torch.from_numpy(np.ctypeslib.as_array(self.outcome, shape=(self.size, 1))).pin_memory().to(device=device, non_blocking=True)
         score = torch.from_numpy(np.ctypeslib.as_array(self.score, shape=(self.size, 1))).pin_memory().to(device=device, non_blocking=True)
-        white = torch._sparse_coo_tensor_unsafe(iw, white_values, (self.size, self.num_inputs))
-        black = torch._sparse_coo_tensor_unsafe(ib, black_values, (self.size, self.num_inputs))
+        white = torch.sparse_coo_tensor(iw, white_values, (self.size, self.num_inputs), check_invariants=False)
+        black = torch.sparse_coo_tensor(ib, black_values, (self.size, self.num_inputs), check_invariants=False)
         white._coalesced_(True)
         black._coalesced_(True)
         return us, them, white, black, outcome, score
